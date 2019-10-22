63556eb62b924391612e90763775eb76c29fce4c
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-10-22 11:41:48 +0200

    Add files via upload

--- site/FAQ.php
+++ site/FAQ.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,8 +7,20 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<?php include("header.php"); ?>
-<h1>FAQ</h1>
+  <?php include("header.php"); ?>
+        
+        <form action="#" method="POST">
+          <h2>Bienvenue dans FAQ</h2>
+          <h3>Questions courantes</h3>
 
+          <h4>Question 1</h4>
+          <p>reponse 1</p>
+           <h4>Question 2</h4>
+          <p>reponse 2</p>
+           <h4>Question 3</h4>
+          <p>reponse 4</p>
+           <h4>Question 5</h4>
+          <p>reponse 5</p>
+        </form>
 </body>
 </html>
