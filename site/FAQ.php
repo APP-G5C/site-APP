26d2544928d32b8797657fe149925c835451969b
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-10-24 12:06:51 +0200

    Add files via upload

--- site/FAQ.php
+++ site/FAQ.php
@@ -7,20 +7,27 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <?php include("header.php"); ?>
-        
+      <div class="bg">
         <form action="#" method="POST">
-          <h2>Bienvenue dans FAQ</h2>
-          <h3>Questions courantes</h3>
+          <h2>BIENVENUE DANS LA FAQ</h2>
+           <div class="line"></div>
 
-          <h4>Question 1</h4>
-          <p>reponse 1</p>
-           <h4>Question 2</h4>
-          <p>reponse 2</p>
-           <h4>Question 3</h4>
-          <p>reponse 4</p>
-           <h4>Question 5</h4>
-          <p>reponse 5</p>
+          <h3>QUESTIONS COURANTES</h3>
+
+
+          <h4>Comment faire pour créer un compte ?</h4>
+          <p>Pour créer votre compte, veuillez suivre les instructions de la FAQ pour la question "Comment faire pour créer un compte ?". Si vous n'arrivez toujours pas a créer votre compte, tant pis.</p>
+           <h4>Comment vous contacter ? </h4>
+          <p>pour nous contacter vous pouvez utilisez la boite mail intégrer au site, pour cela appuyez simplement sur le bouton correspondant.</p>
+           <h4>Comment fonctionne les tests ?</h4>
+          <p>Je ne sais pas.</p>
+           <h4>Comment récuperer mes scores ?</h4>
+          <p>Pour récuperer vos scores veuillez allez dans l'onglet "score" de votre profil qui se trouve quelque part sur le site.</p>
+          <h4>Comment supprimer mon compte ?</h4>
+          <p>Vous ne pouvez pas.</p>
+          <h4>Où doit on effectuer les tests ?</h4>
+          <p>Dans une institut spécialisé comment un truc d'auto école</p>
         </form>
+</div>
 </body>
 </html>
