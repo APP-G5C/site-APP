28310f53aaa7bf9272ef54d33f5fa56aa5e93372
NathanG5C <55877065+NathanG5C@users.noreply.github.com>
2019-11-05 09:54:34 +0100

    Update FAQ.php

--- site/FAQ.php
+++ site/FAQ.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>SmartBeing</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="FAQCSS.css">
 </head>
 <body>
       <div class="bg">
