be2e23fe29762ea5ae6b8351affd8195102de866
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-11-07 12:16:40 +0100

    Add files via upload

---
+++ site/FAQadmin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SmartBeing</title>
+    <link rel="stylesheet" href="FAQadmin.css">
+</head>
+<body>
+ 	<?php include("header.php"); ?>
+   	<div class="bg">
+
+        <form action="#" method="POST">
+          <h2>BIENVENUE DANS LA FAQ</h2>
+           <div class="line"></div>
+
+          <h3>QUESTIONS COURANTES</h3>
+
+
+          <h4>Comment faire pour créer un compte ?</h4>
+          <p>Pour créer votre compte, veuillez suivre les instructions de la FAQ pour la question "Comment faire pour créer un compte ?". Si vous n'arrivez toujours pas a créer votre compte, tant pis.</p>
+           <h4>Comment vous contacter ? </h4>
+          <p>pour nous contacter vous pouvez utilisez la boite mail intégrer au site, pour cela appuyez simplement sur le bouton correspondant.</p>
+           <h4>Comment fonctionne les tests ?</h4>
+          <p>Je ne sais pas.</p>
+           <h4>Comment récuperer mes scores ?</h4>
+          <p>Pour récuperer vos scores veuillez allez dans l'onglet "score" de votre profil qui se trouve quelque part sur le site.</p>
+          <h4>Comment supprimer mon compte ?</h4>
+          <p>Vous ne pouvez pas.</p>
+          <h4>Où doit on effectuer les tests ?</h4>
+          <p>Dans une institut spécialisé comment un truc d'auto école</p>
+
+          <button class="button">Ajouter une question</button>
+        </form>
+</div>
+</body>
+</html>
