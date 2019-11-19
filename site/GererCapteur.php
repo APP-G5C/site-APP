f46ea852137a59b5b39b662bad5214ce72ff2613
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-11-19 11:42:10 +0100

    Add files via upload

---
+++ site/GererCapteur.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SmartBeing</title>
+    <link rel="stylesheet" href="GererCapteur.css">
+</head>
+<body>
+	<?php include("header.php"); ?>
+   	<div class="bg">
+
+   
+
+      <form action="#" method="POST">
+      <div class="data">
+         <div class="colonne1">
+			<h3>Capteurs :</h3><br/>
+			<h4>Fréquence cardiaque</h4><br/>
+			<h4>Témperature de la peau</h4><br/>
+			<h4>Perception auditive</h4><br/>
+			<h4>Reconnaissance de tonalité</h4><br/>
+			<h4>Reproduction de motifs sonors</h4><br/>
+		 </div>
+		 <div class="colonne2" >
+			<h3>Batterie :</h3><br/>
+			<h4>85%</h4><br/>
+			<h4>2%</h4><br/>
+			<h4>44%</h4><br/>
+			<h4>0%</h4><br/>
+			<h4>99%</h4><br/>
+		 </div>
+	   </div>
+	
+		  
+
+          
+ <button class="button"><a style="color:white; text-decoration:none" href="FAQ.html">Ajouter un capteur</a></button>
+        
+      </form>
+</div>
+</body>
+</html>
