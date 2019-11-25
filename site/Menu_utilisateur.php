398b49f1d0689da1342bd34ff3fdae646dc8c5e1
redleader14 <46784287+redleader14@users.noreply.github.com>
2019-11-25 13:52:00 +0100

    Add files via upload

--- site/Menu_utilisateur.php
+++ site/Menu_utilisateur.php
@@ -3,15 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>SmartBeing</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="">
 </head>
 <body>
-    <ul>
-        <li><a href="Tests.php">Tests</a></li>
-        <li><a href="Scores.php">Scores</a></li>
-        <li><a href="Profil.php">Profil</a></li>
-        <li><a href="FAQ.php">FAQ</a></li>
-    </ul>
+<?php include("header.php"); ?>
     
 </body>
 </html>
