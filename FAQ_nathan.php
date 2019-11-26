f52a46bf52b88548f5286033d86bd24fb713d4ad
NathanG5C <55877065+NathanG5C@users.noreply.github.com>
2019-11-26 10:44:54 +0100

    Add files via upload

---
+++ FAQ_nathan.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SmartBeing</title>
+    <link rel="stylesheet" href="FAQCSS.css">
+</head>
+<body>
+  <?php include("header.php"); ?>
+      <div class="bg">
+        <form action="#" method="POST">
+          <h2>FAQ</h2>
+           <div class="line"></div>
+
+          <h3>QUESTIONS COURANTES</h3>
+
+
+          <h4>Comment faire pour créer un compte ?</h4>
+          <p>à remplir
+           <h4>Comment nous contacter ? </h4>
+          <p>pour nous contacter vous pouvez utilisez la boite mail intégrer au site, pour cela appuyez simplement sur le bouton correspondant.</p>
+           <h4>Comment fonctionne les tests ?</h4>
+          <p>Nos appareils sont disponibles uniquement pour les ayant-droit, ceux-ci utilisent des capteurs et dispositfs permettant d'effectuer les tests. Les résultats sont ensuite directement transmis sur ce site web. </p>
+           <h4>Comment récuperer mes scores ?</h4>
+          <p>Pour récuperer vos scores veuillez allez dans l'onglet "score" de votre profil qui se trouve quelque part sur le site.</p>
+          <h4>Comment supprimer mon compte ?</h4>
+          <p>Vous ne pouvez pas.</p>
+          <h4>Où doit on effectuer les tests ?</h4>
+          <p>Dans un institut spécialisé tel qu'une auto-école, un hopital etc. </p>
+        </form>
+</div>
+
+<style>
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  opacity: 0.5; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+  opacity : .5;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+  opacity: 0.5;
+
+}
+</style>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<ul>
+    <li><a href="Tests.php">Tests</a></li>
+    <li><a href="Scores.php">Scores</a></li>
+    <li><a href="Profil.php">Profil</a></li>
+    <li><a href="FAQ.php">FAQ</a></li>
+</ul>
+
+</body>
+</html>
