e050d4b3a6db39030b7854c95c254de2b800c581
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-02 14:19:51 +0100

    Add files via upload

---
+++ Menu_nathan.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="menu_utilisateur_css.css">
+	<title>Accueil</title>
+</head>
+<body background="Background.jpg">
+	
+<div id="conteneur">
+	<div class="element"><a href="www.facebook.com"></a>Test <br/> <img src='bouton_play.png'> </div>
+	<div class="element"><a href="www.?"></a> Scores <br/> <img src='bouton_score.png'></div>
+	<div class="element"><a href="www.?"></a> Profil <br/> <img src='profil.png'></div>
+	<div class="element"><a href="www.?"></a>FAQ <br/> <img src='faq.png'></div>
+</div>	
+
+</body>
+</html>
