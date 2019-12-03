0e9cef0ce08c7241cd46b259920d3b9d6d8085a4
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-03 09:11:43 +0100

    Add files via upload

---
+++ user.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>SmartBeing</title>
+    <link rel="stylesheet" href="user.css">
+</head>
+<body>
+<?php include("headerAdmin.php"); ?>
+   <div class="bg">
+        <form action="#" method="POST">
+          <h2>Utilisateur</h2>
+           <div class="line"></div>
+           
+            <button class="button"><a style="color:white; text-decoration:none" href="enregistrement.php">Rechercher un utilisateur</a></button>
+          <button class="button"><a style="color:white; text-decoration:none" href="enregistrement.php">Créer un compte</a></button>
+        </form>
+  </div>
+</body>
+</html>
