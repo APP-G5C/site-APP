f42345bb8d1c985f8f44619c7eed4721b4464b6a
Stan-Le-Floch <55876315+Stan-Le-Floch@users.noreply.github.com>
2019-12-03 10:36:26 +0100

    Add files via upload

--- Scores.php
+++ Scores.php
@@ -1,13 +1,75 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>SmartBeing</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-<?php include("header.php"); ?>
-    <h1>Votre score général:</h1>
-    
-</body>
+      <head>
+        <neta charset="utf-8"/>
+        <link rel="stylesheet" href="score.css"/>
+        
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Scores</title>
+    </head>
+
+    <body>
+      <?php include("header.php"); ?>
+        <div class="bg">
+          <form action="#" method="POST">
+
+        <h1>Jean DUPONT</h1>
+        <h4> Utilisateur depuis : Novembre 2020</h4>
+        <h4> Tests realises : 152 </h4>
+        <table>
+
+
+   <thead>
+       <tr>
+           <th>Test</th>
+           <th>Score moyen</th>
+           <th>Meilleur score</th>
+           <th>Pire score</th>
+
+       </tr>
+   </thead>
+
+
+   <tbody> <!-- Corps du tableau -->
+       <tr>
+           <td>Frequence cardiaque</td>
+           <td>62</td>
+           <td>70</td>
+           <td>58</td>
+       </tr>
+       <tr>
+           <td>Temperature de la peau</td>
+           <td>73</td>
+           <td>77</td>
+           <td>69</td>
+       </tr>
+       <tr>
+           <td>Perception de la peau</td>
+           <td>60</td>
+           <td>67</td>
+           <td>52</td>
+       </tr>
+       <tr>
+           <td>Reconnaissance de tonalite</td>
+           <td>46</td>
+           <td>55</td>
+           <td>41</td>
+       </tr>
+       <tr>
+           <td>Reproduction de motifs sonores</td>
+           <td>56</td>
+           <td>65</td>
+           <td>42</td>
+       </tr>
+      
+   </tbody>
+
+</table>
+        </form>
+        </div>
+    </body>
 </html>
+
+
